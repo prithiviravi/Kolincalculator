fd3c327f32f793ad1fab843a03daed887fc8fde7
prithiviravi <80674299+prithiviravi@users.noreply.github.com>
2021-10-13 08:57:11 +0530

    Update views.py

--- kolin8/views.py
+++ kolin8/views.py
@@ -140,7 +140,7 @@ def calculate(request):
 
     Cholinechloridedose = Cholinedefeciency * 2.222 
     
-    KOLINDOSE = Cholinedefeciency * 0.8
+    KOLINDOSE = Cholinedefeciency * 0.25
 
     if(TotalValue <= 100):
         return render(request,'result.html', {'Result':result, 'Cholinerequired':Cholinerequired, 'Cholinedefeciency':Cholinedefeciency, 'cholinecloridedose':Cholinechloridedose, 'KD':KOLINDOSE})    
